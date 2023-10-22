86e0d41a4c7fcaf03956da0f45d39f63fa148116
Artur Fejklowicz <artur.fejklowicz@gmail.com>
2023-10-22 23:28:27 +0200

    adds forgotten sort

--- python/logistic_regression_classification/main.py
+++ python/logistic_regression_classification/main.py
@@ -1,4 +1,12 @@
+__author__ = "Artur Fejklowicz"
+
+# Sources:
+# - https://medium.com/@ashins1997/text-classification-dfe370bf7044
+# - https://www.kaggle.com/code/shahkan/text-classification-using-logistic-regression
+
 import pandas as pd
+from sklearn.model_selection import cross_val_score, cross_validate
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from google.cloud import bigquery
 
@@ -12,9 +20,22 @@ y = df["Konto"]
 # Training data
 X = df.drop("Konto", axis=1)
 
+# Change text to numbers
+le = LabelEncoder()
+for col in X.columns:
+    X[col] = le.fit_transform(X[col])
+
+
 # Train model
 model = LogisticRegression()
-model.fit(X, y)
+#model.fit(X, y)
+scores = cross_val_score(model, X, y, cv=10)
+print(scores.mean())
+
+results = cross_validate(model, X, y, scoring="accuracy", cv=10)
+print(results["test_score"].mean())
+model = results["best_estimator_"]
+print(results["best_estimator_"])
 
 # Test model
 X_test = pd.DataFrame({"tekst": ["GoodLood", "Restauracja Pajda", "AAABBB"]})
